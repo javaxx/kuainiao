d04fe45e59dd50231bc272c0e5fae889d70e816f
javaxx <136222866@qq.com>
2017-08-21 02:37:08 +0800

    init

--- app/UserOpenid.php
+++ app/UserOpenid.php
@@ -13,7 +13,7 @@ class UserOpenid extends Model
 
     public function user()
     {
-        return $this->hasOne(\App\User::class,'id','user_id');
+        return $this->hasOne(\App\User::class,'user_id','d');
 
     }
 }
