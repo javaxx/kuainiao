ea13a66bd1b4b36ef2d4194759481643e3b194c1
javaxx <136222866@qq.com>
2017-08-21 03:29:59 +0800

    init

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -18,7 +18,7 @@
                     <p class="blog-post-meta">{{$post->created_at->toFormattedDateString()}}          by     <mark>{{$post->user->name}}</mark></p>
                     <p class="blog-post-meta">  浏览  {{ count($post->visitors) }}  |  兑换 {{$post->buys_count}}    |   赞 {{$post->zans_count}}  | 评论 {{$post->comments_count}}  </p>
 
-                    <p><a class="btn btn-default" href="#" role="button">点我下载</a></p>
+                    <p><a class="btn btn-default" href="/posts/{{$post->id}}"  role="button">点我下载</a></p>
                 </div>
 
 
