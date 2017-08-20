bae71f39eccd52af03fda997254bd083b231e083
javaxx <136222866@qq.com>
2017-08-21 02:54:54 +0800

    init

--- app/UserOpenid.php
+++ app/UserOpenid.php
@@ -13,6 +13,6 @@ class UserOpenid extends Model
 
     public function user()
     {
-        return $this->belongsTo(\App\User::class);
+        return $this->belongsTo(\App\User::class,'user_id','id');
     }
 }
