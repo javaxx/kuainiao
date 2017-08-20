952175aeacb9831538e943bf5c9478f48c7d659b
javaxx <136222866@qq.com>
2017-08-20 22:37:30 +0800

    init

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -9,6 +9,8 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0 user-scalable=no">
         <meta name="renderer" content="webkit">
         <meta http-equiv="Cache-Control" content="no-siteapp">
         <meta http-equiv="Cache-Control" content="no-transform ">
