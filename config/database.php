bd45cd10663362eabc9dbe581fdbf319ea91e158
javaxx <136222866@qq.com>
2017-08-23 23:08:02 +0800

    init

--- config/database.php
+++ config/database.php
@@ -53,7 +53,7 @@ return [
             'strict' => true,
             'engine' => null,
             'dump' => [
-                'dump_binary_path' => '/usr/bin/', // only the path, so without `mysqldump` or `pg_dump`
+                'dump_binary_path' => 'D:\laragon\bin\mysql\mysql-5.7.19-winx64\bin', // only the path, so without `mysqldump` or `pg_dump`
                 'use_single_transaction',
                 'timeout' => 60 , // 5 minute timeout
                 'exclude_tables' => [''],
