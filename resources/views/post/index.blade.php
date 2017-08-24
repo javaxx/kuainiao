b5bcdd0f77e417471f81d876c43dac1046b5ca43
javaxx <136222866@qq.com>
2017-08-24 16:40:15 +0800

    init

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -8,21 +8,23 @@
     <div class="container summary">
 
         <!-- 简介 -->
-        <div class="row" id="summary-container">
-            @foreach($posts as $post)
-                <div class="col-md-4">
+        @foreach($posts->chunk(4) as $chunk)
 
+        <div class="row" id="summary-container">
+            @foreach($chunk as $post)
+
+               <div class="col-md-3">
                     <h2><a href="/posts/{{$post->id}}" >{{$post->title}}</a></h2>
                     <p class="blog-post-meta">{{$post->created_at->toFormattedDateString()}}          by     <mark>{{$post->user->name}}</mark></p>
                     <p class="blog-post-meta">  浏览  {{ count($post->visitors) }}  |  兑换 {{$post->buys_count}}    |   赞 {{$post->zans_count}}  | 评论 {{$post->comments_count}}  </p>
-
                     <p><a class="btn btn-default" href="/posts/{{$post->id}}"  role="button">点我下载</a></p>
                 </div>
-
-
-
             @endforeach
+
+
+
         </div>
+        @endforeach
 
         </div>
 
