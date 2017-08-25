8b6f930c57e4bba00804789e0d6f3596397f1150
javaxx <136222866@qq.com>
2017-08-25 12:48:47 +0800

    init

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -17,7 +17,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::aviable()->orderBy('created_at', 'desc')->withCount(["zans", "comments",'buys'])->with(['user'])->paginate(6);
+        $posts = Post::aviable()->orderBy('created_at', 'desc')->withCount(["zans", "comments",'buys'])->with(['user'])->paginate(8);
 
         return view('post/index', compact('posts'));
     }
